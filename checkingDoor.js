2e9e8772286d3b2f2530110f48317c488fef10df
Tonkata28 <tonisimeonov33@gmail.com>
2025-01-12 17:02:01 +0200

    Add files via upload

---
+++ checkingDoor.js
@@ -0,0 +1,11 @@
+function doorChecking(doorNum){
+    if (doorNum === 3){
+        console.log(`Congratulations, Pen4o! You found the treasure behind Door ${doorNum}!`)
+    } else if (doorNum === 2 || doorNum === 1){
+        console.log(`A bucket of cold water splashes on you! Unlucky Pen4o!`);
+    } else {
+        console.log("Invalid choice. Please select Door 1, 2, or 3.");
+        
+    }
+}
+doorChecking()
