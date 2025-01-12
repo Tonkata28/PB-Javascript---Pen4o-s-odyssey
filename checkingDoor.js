07c8eafa10c6fe22670caeda2e0d847b79280bf7
Tonkata28 <tonisimeonov33@gmail.com>
2025-01-12 17:32:08 +0200

    Update checkingDoor.js

--- checkingDoor.js
+++ checkingDoor.js
@@ -1,10 +1,15 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
 function doorChecking(doorNum){
-    if (doorNum === 3){
+    let randomNum = getRandomInt(1, 3);
+    if (doorNum === randomNum){
         console.log(`Congratulations, Pen4o! You found the treasure behind Door ${doorNum}!`)
-    } else if (doorNum === 2 || doorNum === 1){
+    } else if (doorNum !== randomNum){
         console.log(`A bucket of cold water splashes on you! Unlucky Pen4o!`);
     } else {
         console.log("Invalid choice. Please select Door 1, 2, or 3.");
     }
+    console.log(randomNum)
 }
 doorChecking()
